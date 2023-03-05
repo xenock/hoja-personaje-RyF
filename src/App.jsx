157edd3568e5009bed86783417c622ebc65dbb1c
Xenock <miguelmoracho@gmail.com>
2023-03-05 12:15:55 +0100

    feat: added nested form to attributes and abilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import '@picocss/pico'
+import { useState, useEffect } from 'react'
+import { abilities, attributes, rasgos, config } from './db'
+import { useForm } from 'react-hook-form'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main className="container-fluid">
+      <header>
+        <section style={{ columns: 4 }}>
+          {Object.entries(attributes).map(([key, { label }]) => (
+            <>
+              <label htmlFor={key}>{label}</label>
+              <input
+                name={key}
+                placeholder={label}
+                {...register(key)}
+                type="number"
+                min={config.attributes.min}
+                max={config.attributes.max}
+                defaultValue={config.attributes.min}
+              />
+            </>
+          ))}
+        </section>
+      </header>
+      <section style={{ columns: 3 }}>
+        {abilities.actual.map(({ key, label, attribute }) => (
+          <article>
+            <label htmlFor={key}>{label}</label>
+            <input
+              name={key}
+              placeholder={label}
+              {...register(key)}
+              type="number"
+              min={config.abilities.min}
+              max={config.abilities.max}
+              defaultValue={config.abilities.min}
+            />
+            <p>{`${
+              Number(watch(attribute, config.abilities.min)) +
+              Number(watch(key, 0))
+            }`}</p>
+          </article>
+        ))}
+      </section>
+    </main>
   )
 }
 
---
+++ src/db.js
@@ -0,0 +1,78 @@
+const actual = [
+  { attribute: 'perception', label: 'Advertir/Notar', key: 'warn' },
+  { attribute: 'dexterity', label: 'Armas a Distancia', key: 'remoteWeapons' },
+  {
+    attribute: 'physical',
+    label: 'Armas Cuerpo a Cuerpo',
+    key: 'meleeWeapons',
+  },
+  { attribute: 'physical', label: 'Atletismo', key: 'athletics' },
+  { attribute: 'perception', label: 'Bailar', key: 'dance' },
+  { attribute: 'perception', label: 'Buscar', key: 'lookFor' },
+  { attribute: 'intelligence', label: 'Callejeo', key: 'callejeo' },
+  { attribute: 'intelligence', label: 'Comercio', key: 'trade' },
+  { attribute: 'dexterity', label: 'Conducir', key: 'drive' },
+  { attribute: 'dexterity', label: 'Esquivar', key: 'dodge' },
+  { attribute: 'intelligence', label: 'Historia', key: 'history' },
+  { attribute: 'intelligence', label: 'Idiomas', key: 'languages' },
+  { attribute: 'intelligence', label: 'Informática', key: 'computing' },
+  { attribute: 'intelligence', label: 'Leyes', key: 'laws' },
+  { attribute: 'intelligence', label: 'Mecánica', key: 'mechanics' },
+  { attribute: 'intelligence', label: 'Medicina', key: 'medicine' },
+  { attribute: 'perception', label: 'Música', key: 'music' },
+  { attribute: 'intelligence', label: 'Ocultismo', key: 'occultism' },
+  { attribute: 'perception', label: 'Reflejos', key: 'reflexes' },
+  { attribute: 'dexterity', label: 'Pilotar', key: 'pilot' },
+  { attribute: 'dexterity', label: 'Sigilo', key: 'stealth' },
+  { attribute: 'intelligence', label: 'Supervivencia', key: 'survival' },
+]
+
+export const abilities = { actual }
+
+export const config = {
+  attributes: {
+    min: 4,
+    max: 10,
+  },
+  abilities: {
+    min: 0,
+    max: 10,
+  },
+  points: {
+    realistic: 22,
+    normal: 30,
+  },
+  pyramid: {
+    specialistHeroic: [3, 3, 3, 3, 3, 1],
+    versatileHeroic: [6, 5, 4, 3, 2, 1],
+    specialistRealistic: [2, 2, 2, 2, 2],
+    versatileRealistic: [5, 4, 3, 2, 1],
+  },
+}
+
+export const attributes = {
+  perception: {
+    label: 'Percepción',
+  },
+  intelligence: {
+    label: 'Inteligencia',
+  },
+  dexterity: {
+    label: 'Destreza',
+  },
+  physical: {
+    label: 'Físico',
+  },
+}
+
+export const rasgos = [
+  { nombre: 'Nombre', longitud: 'corto' },
+  { nombre: 'Altura', longitud: 'corto' },
+  { nombre: 'Ojos', longitud: 'corto' },
+  { nombre: 'Edad', longitud: 'corto' },
+  { nombre: 'Peso', longitud: 'corto' },
+  { nombre: 'Pelo', longitud: 'corto' },
+  { nombre: 'Lugar de nacimiento', longitud: 'largo' },
+  { nombre: 'Ocupacion', longitud: 'largo' },
+  { nombre: 'Descripcion', longitud: 'largo' },
+]

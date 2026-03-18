1c909362b5799482a2c3cea914c66b095358e966
xenock <miguelmoracho@gmail.com>
2026-03-18 10:47:36 +0100

    refactor: introduced form provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import '@picocss/pico'
 import { Fragment } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, FormProvider } from 'react-hook-form'
 import { abilities, attributes, config } from './db'
 import styles from './style.module.css'
 
 function App() {
+    const methods = useForm()
   const {
     register,
     handleSubmit,
     getValues,
     watch,
     formState: { errors },
-  } = useForm()
+  } = methods
 
   const group = (entries) => {
     if (!entries) return null
@@ -24,87 +25,87 @@ function App() {
   }
 
   return (
-    <main className="container-fluid">
-      <header>
-        <section className={styles.attributes}>
-          {Object.entries(attributes).map(([key, { label }], index) => (
-            <label htmlFor={key} key={index}>
-              {label}
-              <input
-                name={key}
-                placeholder={label}
-                {...register(`attributes.${key}`)}
-                type="number"
-                min={config.attributes.min}
-                max={config.attributes.max}
-                defaultValue={config.attributes.min}
-              />
-            </label>
-          ))}
-        </section>
-      </header>
-      <section>
-        <fieldset>
-          <legend>Tono de la partida</legend>
-          {Object.entries(config.pyramid).map(([key, { label }], index) => (
-            <label htmlFor={key} key={index}>
-              <input type="radio" {...register('pyramid')} value={key} />
-              {label}
-            </label>
-          ))}
-        </fieldset>
-        <div className="grid">
-          {config.pyramid[watch('pyramid')]?.values.map((value, index) => (
-            <div>
-              <p
-                className={`${
-                  group(getValues('abilities'))[index + 1]?.length === value
-                    ? styles.limit
-                    : ''
-                }
-                  ${
-                    group(getValues('abilities'))[index + 1]?.length > value
+    <FormProvider {...methods}>
+      <main className="container-fluid">
+        <header>
+          <section className={styles.attributes}>
+            {Object.entries(attributes).map(([key, { label }], index) => (
+              <label htmlFor={key} key={index}>
+                {label}
+                <input
+                  name={key}
+                  placeholder={label}
+                  {...register(`attributes.${key}`)}
+                  type="number"
+                  min={config.attributes.min}
+                  max={config.attributes.max}
+                  defaultValue={config.attributes.min}
+                />
+              </label>
+            ))}
+          </section>
+        </header>
+        <section>
+          <fieldset>
+            <legend>Tono de la partida</legend>
+            {Object.entries(config.pyramid).map(([key, { label }], index) => (
+              <label htmlFor={key} key={index}>
+                <input type="radio" {...register('pyramid')} value={key} />
+                {label}
+              </label>
+            ))}
+          </fieldset>
+          <div className="grid">
+            {config.pyramid[watch('pyramid')]?.values.map((value, index) => (
+              <div>
+                <p
+                  className={`${group(getValues('abilities'))[index + 1]?.length === value
+                      ? styles.limit
+                      : ''
+                    }
+                  ${group(getValues('abilities'))[index + 1]?.length > value
                       ? styles.off
                       : ''
-                  }`}
-              >
-                {value} habilidades de nivel {index + 1} :
-              </p>
-              <ul>
-                {group(getValues('abilities'))[index + 1]?.map((key) => (
-                  <li>
-                    {
-                      abilities.actual.find((ability) => ability.key === key)
-                        .label
-                    }
-                  </li>
-                ))}
-              </ul>
-            </div>
+                    }`}
+                >
+                  {value} habilidades de nivel {index + 1} :
+                </p>
+                <ul>
+                  {group(getValues('abilities'))[index + 1]?.map((key) => (
+                    <li>
+                      {
+                        abilities.actual.find((ability) => ability.key === key)
+                          .label
+                      }
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className={styles.abilities}>
+          {abilities.actual.map(({ key, label, attribute }, index) => (
+            <Fragment key={index}>
+              <label htmlFor={key} key={index}>
+                <input
+                  name={key}
+                  placeholder={label}
+                  {...register(`abilities.${key}`)}
+                  type="number"
+                  min={config.abilities.min}
+                  max={config.abilities.max}
+                  defaultValue={config.abilities.min}
+                />
+                {`${label} `}
+                {Number(watch(`attributes.${attribute}`, config.attributes.min)) +
+                  Number(watch(`abilities.${key}`, config.abilities.min))}
+              </label>
+            </Fragment>
           ))}
-        </div>
-      </section>
-      <section className={styles.abilities}>
-        {abilities.actual.map(({ key, label, attribute }, index) => (
-          <Fragment key={index}>
-            <label htmlFor={key} key={index}>
-              <input
-                name={key}
-                placeholder={label}
-                {...register(`abilities.${key}`)}
-                type="number"
-                min={config.abilities.min}
-                max={config.abilities.max}
-                defaultValue={config.abilities.min}
-              />
-              {`${label} `}
-              {Number(watch(`attributes.${attribute}`, config.attributes.min)) +
-                Number(watch(`abilities.${key}`, config.abilities.min))}
-            </label>
-          </Fragment>
-        ))}
-      </section>
-    </main>
+        </section>
+      </main>
+    </FormProvider>
   )
 }
 

47aab7725a3ef14281e133363fefe76c59380e0a
xenock <miguelmoracho@gmail.com>
2026-03-19 12:57:17 +0100

    chore: Configure Vitest to use global APIs in tests.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts']
   }
